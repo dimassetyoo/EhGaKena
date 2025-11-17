e3a157e8d8be8b762346332f57631f60c466b54b
dimassetyoo <dimassetyoo@gmail.com>
2025-11-17 14:22:58 +0700

    ObjHealth, ObjScore, dan Canvas

---
+++ Assets/Game/Scripts/ObjScore.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ObjScore : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
